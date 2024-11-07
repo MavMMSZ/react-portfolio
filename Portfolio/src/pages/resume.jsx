74f81cdd6addacf80ec18044e95fee1d865ddf5b
Maverick S <maverickmmsc@gmail.com>
2024-11-06 21:10:40 -0500

    pages updates

---
+++ Portfolio/src/pages/resume.jsx
@@ -0,0 +1,23 @@
+export default function resume() {
+    return (
+        <><div>
+            Download my resume
+        </div><div>
+                <h1>Front End Proficencies</h1>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>jQuery</li>
+                </ul>
+                <h1>Back End Proficencies</h1>
+                <ul>
+                    <li>Node.js</li>
+                    <li>Express</li>
+                    <li>APIs</li>
+                    <li>MySQL</li>
+                </ul>
+            </div></>
+    );
+}
