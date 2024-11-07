426e567090ed683618c866697b791ec48020181f
Maverick S <maverickmmsc@gmail.com>
2024-11-06 20:04:04 -0500

    app and main rough drafts in

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
+import App from './App.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
+import aboutMe from './pages/aboutMe.jsx'
+import contact from './pages/contact.jsx'
+import portfolio from './pages/portfolio.jsx'
+import resume from './pages/resume.jsx'
+import React from 'react';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <aboutMe />,
+      },
+      {
+        path: '/contact',
+        element: <contact />,
+      },
+      {
+        path: '/portfolio',
+        element: <portfolio />,
+      },
+      {
+        path: '/resume',
+        element: <resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# react-portfolio
+# Portfolio w/ React
