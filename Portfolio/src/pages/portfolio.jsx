f6a49d9438295936496aa09711c32f5935ac6a62
Maverick S <maverickmmsc@gmail.com>
2024-11-11 17:56:02 -0500

    got all images

--- Portfolio/src/pages/portfolio.jsx
+++ Portfolio/src/pages/portfolio.jsx
@@ -4,12 +4,12 @@ import Project from "../components/project.jsx";
 import "../styles/portfolio.css";
 
 const projects = [
-    { title: "Project 1", image: "src/assets/Capture.PNG" },
-    { title: "Project 2", image: "src/assets/Capture.PNG" },
-    { title: "Project 3", image: "src/assets/Capture.PNG" },
-    { title: "Project 4", image: "src/assets/Capture.PNG" },
-    { title: "Project 5", image: "src/assets/Capture.PNG" },
-    { title: "Project 6", image: "src/assets/Capture.PNG" },
+    { title: "Weather Dashboard", image: "src/assets/Capture.PNG" },
+    { title: "Employee Tracker", image: "src/assets/tracker.PNG" },
+    { title: "README Generator", image: "src/assets/readme.PNG" },
+    { title: "Vacation Calculator", image: "src/assets/calc.PNG" },
+    { title: "Vehicle Builder", image: "src/assets/builder.PNG" },
+    { title: "Park Finder", image: "src/assets/finder.PNG" },
 ];
 
 export default function Portfolio() {
