e5afa745f6d4ff3a4cf1ba4ef267f0b2dcd5ad96
Maverick S <maverickmmsc@gmail.com>
2024-11-11 18:17:57 -0500

    finished aboutMe

--- Portfolio/src/pages/aboutMe.jsx
+++ Portfolio/src/pages/aboutMe.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
+
+
 export default function AboutMe() {
   return (
     <div class='about'>
       <h1>About Me</h1>
-      <p>
+      <div class='about-content'> 
+      <img class='mav' src='src/assets/mavy.jpg' alt='Maverick Snider' />
+      <p class='me'>
         Hello! My name is Maverick Snider and I am a learning web developer. I am currently enrolled in the university of Ohio's coding bootcamp. I am learning how to code in HTML, CSS, and JavaScript. I am also learning how to use React and Node.js. I am excited to learn more about coding and to start my career as a web developer.
       </p>
+      </div>
     </div>
   );
 }
