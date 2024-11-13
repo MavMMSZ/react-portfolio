c0bcda1841406d8d457550a1bbe28891e75492f4
Maverick S <maverickmmsc@gmail.com>
2024-11-13 00:04:05 -0500

    deploy fixes x5

--- Portfolio/src/pages/portfolio.jsx
+++ Portfolio/src/pages/portfolio.jsx
@@ -4,12 +4,12 @@ import Project from "../components/project.jsx";
 import "../styles/portfolio.css";
 
 const projects = [
-    { title: "Weather Dashboard", image: "../public/assets/Capture.PNG", url:"https://github.com/MavMMSZ/weather-dashboard" },
-    { title: "Employee Tracker", image: "../public/assets/tracker.PNG", url:"https://github.com/MavMMSZ/employee-tracker" },
-    { title: "README Generator", image: "../public/assets/readme.PNG", url:"https://github.com/MavMMSZ/readMeGenerator" },
-    { title: "Vacation Calculator", image: "../public/assets/calc.PNG", url:"https://github.com/HarrisCH258/Vacation-Budget-Calculator" },
-    { title: "Vehicle Builder", image: "../public/assets/builder.PNG", url:"https://github.com/MavMMSZ/vehicle-builder" },
-    { title: "React Portfolio", image: "../public/assets/react.PNG", url:"https://github.com/MavMMSZ/react-portfolio" },
+    { title: "Weather Dashboard", image: "/assets/Capture.PNG", url:"https://github.com/MavMMSZ/weather-dashboard" },
+    { title: "Employee Tracker", image: "/assets/tracker.PNG", url:"https://github.com/MavMMSZ/employee-tracker" },
+    { title: "README Generator", image: "/assets/readme.PNG", url:"https://github.com/MavMMSZ/readMeGenerator" },
+    { title: "Vacation Calculator", image: "/assets/calc.PNG", url:"https://github.com/HarrisCH258/Vacation-Budget-Calculator" },
+    { title: "Vehicle Builder", image: "/assets/builder.PNG", url:"https://github.com/MavMMSZ/vehicle-builder" },
+    { title: "React Portfolio", image: "/assets/react.PNG", url:"https://github.com/MavMMSZ/react-portfolio" },
 ];
 
 export default function Portfolio() {
--- Portfolio/src/pages/resume.jsx
+++ Portfolio/src/pages/resume.jsx
@@ -3,7 +3,7 @@ export default function resume() {
         <div class='resume'>
             <h1><strong>Resume</strong></h1>
             <div>
-                <a href="../public/assets/res.webp" target="_blank" rel="noopener noreferrer">
+                <a href="/assets/res.webp" target="_blank" rel="noopener noreferrer">
                     Download my resume
                 </a>
             </div>
